5c0922cae14e130779009f59c60db30fe866bee4
vitalya0729 <36700292+vitalya0729@users.noreply.github.com>
2018-03-01 03:49:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+      let x = [];
+    
+    let array = equation.replace('x^2', '').replace(/\s/g, '');
+
+         array = array.match(/[-+]*\s*[0-9]+/g);
+
+
+    let   a = array[0];
+    let   b = array[1];
+    let   c = array[2];
+    
+    let D = Math.round (Math.sqrt(Math.pow(b,2) - 4 * a * c));
+
+    x[0]=(-b - D) / (2 * a);
+    x[1]= (-b + D) / (2 * a);
+
+    x.sort((l, r) => l - r);
+    
+    return x;
 }
